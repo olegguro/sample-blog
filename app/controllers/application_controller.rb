5c5dd5809b4553b37627146717cd572b4f365a81
Oleg Guro <ooleg6334@gmail.com>
2019-02-18 21:48:01 +0300

    Add username for sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+
+before_filter :configure_permitted_parameters, if: :device_controller?
+
+    private
+    def configure_permitted_parameters
+        device_parameter_sanitizer.for(:sign_up) << :username 
+    end
+
+
+
+
 end
